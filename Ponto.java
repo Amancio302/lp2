f4ba21ead2c52046f4b901a987d647a6b9fce956
amancio302 <matheusamancioferreira@gmail.com>
2017-02-23 10:22:14 -0300

    Adicionado Reta

--- Ponto.java
+++ Ponto.java
@@ -6,7 +6,7 @@ public class Ponto{
 	public Ponto(double x, double y){
 		this.x = x;
 		this.y = y;
-		System.out.println("Contrutor 2 - Valores: X: " + this.x + " Y: " + this.y);
+		System.out.println("Contrutor 2 - Valores: \nX: " + this.x + "\nY: " + this.y);
 	}
 	public void setX(double x){
 		this.x = x;
@@ -32,5 +32,11 @@ public class Ponto{
 		x12 = p1.getX() - p2.getX();
 		distancia = Math.sqrt((x12*x12)+(y12*y12));
 		System.out.println("Distancia entre os pontos p1 e p2 é: " + distancia);
+		Reta r1 = new Reta();
+		r1.setP1(p1);
+		r1.setP2(p2);
+		p1.setX(30);
+		r1.getP2().setY(40);
+		System.out.println("r1.p1.x = " + r1.getP1().getX() + "\nr1.p1.y = " + r1.getP1().getY() + "\nr1.p2.x = " + r1.getP2().getX() + "\nr1.p2.y = " + r1.getP2().getY());
 	}
 }
