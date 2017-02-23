133460638841273f2d4450af0e503afb7b8046c3
amancio302 <matheusamancioferreira@gmail.com>
2017-02-23 11:47:56 -0300

    Adicionada função sobarea() na classe Reta

--- Ponto.java
+++ Ponto.java
@@ -1,8 +1,8 @@
 public class Ponto{
 	private double x, y;
 	public Ponto(){
-		System.out.println("Construtor 1" + this);
-	}	
+		System.out.println("Construtor 1 " + this);
+	}
 	public Ponto(double x, double y){
 		this.x = x;
 		this.y = y;
@@ -20,11 +20,9 @@ public class Ponto{
 	public double getY(){
 		return this.y;
 	}
-	
-	
 	public static void main(String[] args){
-		Ponto p1 = new Ponto(1, 4);
-		Ponto p2 = new Ponto(3, 2);
+		Ponto p1 = new Ponto(20, 15);
+		Ponto p2 = new Ponto(30, 10);
 		Ponto p3 = new Ponto();
 		Ponto p4 = new Ponto();
 		double distancia, y12, x12;
@@ -35,8 +33,8 @@ public class Ponto{
 		Reta r1 = new Reta();
 		r1.setP1(p1);
 		r1.setP2(p2);
-		p1.setX(30);
-		r1.getP2().setY(40);
 		System.out.println("r1.p1.x = " + r1.getP1().getX() + "\nr1.p1.y = " + r1.getP1().getY() + "\nr1.p2.x = " + r1.getP2().getX() + "\nr1.p2.y = " + r1.getP2().getY());
+		double a = 	r1.sobarea();
+		System.out.println(a);
 	}
 }
