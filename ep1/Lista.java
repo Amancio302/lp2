64c018b2567d376950a8b92c643ae10f0052ab16
amancio302 <matheusamancioferreira@gmail.com>
2017-03-02 17:03:53 -0300

    Failed "inserir"

--- ep1/Lista.java
+++ ep1/Lista.java
@@ -1,46 +1,73 @@
 class No{
-	private int valor;
-	private No proximo, anterior;
-	public void inserirValor(int valor){
-		this.valor = valor;
+	private int x;
+	private No prox, antes;
+	public No(){
+		this.antes = null;
+		this.prox = null;
 	}
-	public void inserirProximo(No proximo){
-		this.proximo = proximo;
+	public void setX(int x){
+		this.x = x;
 	}
-	public void inserirAnterior(No anterior){
-		this.anterior = anterior;
+	public void setProx(No prox){
+		this.prox = prox;
 	}
-	public int retornarValor(){
-		return this.valor;
+	public void setAntes(No antes){
+		this.antes = antes;
 	}
-	public No retornarProximo(){
-		return this.proximo;
+	public int getX(){
+		return this.x;
 	}
-	public No retornarAnterior(){
-		return this.anterior;
+	public No getProx(){
+		return this.prox;
+	}
+	public No getAntes(){
+		return this.antes;
 	}
 }
 
-public class Lista{
-	private No primeiro = new No, ultimo = new No, valor = new No;
-	public void inserir(int valor){
-		No auxiliar = new No;		
-		if(primeiro.retornarProximo() == null){
-			primeiro.inserirProximo(this.valor);
-		}
-		this.valor.inserirValor(valor);
-		auxiliar.inserirAnterior(ultimo.retornarAnterior());
-		ultimo.inserirAnterior(this.valor);
-		auxiliar.inserirProximo(ultimo);
+class Lista{
+	No topo;
+	No calda;
+	int cont;
+	public Lista(){
+		this.topo = new No();
+		this.calda = new No();
+		this.cont = 0;
+		this.topo = null;
+		this.calda = null;
 	}
-	public void exibirLista(){
-		No auxiliar = new No;
-		auxiliar.inserirValor(primeiro.retornarValor());
-		auxiliar.inserirProximo(primeiro.retornarProximo());
-		while(auxiliar.retornarProximo() != null){
-			System.out.println(auxiliar.retornarValor() + ", ");
-			auxiliar.inserirValor(auxiliar.retornarProximo().retornarValor());
-			auxiliar.inserirProximo(auxiliar.retornarProximo().retornarProximo());
+	public void inserir(int x){
+		No aux = new No();
+		No aux2 = new No();
+		aux.setX(x);
+		cont++;
+		if(topo == null){
+			topo = aux;
+			calda = aux;
+		}
+		else{
+			aux2 = topo.getProx();
+			for(int i = 0; i < cont; i++){
+				if(aux2.getProx() == null){
+					aux2.setProx(aux);
+				aux.setAntes(aux2);
+				aux.setProx(null);
+				calda = aux;
+				}
+				else{
+					aux2 = aux2.getProx();
+				}
+			}
+		}
+	}
+	public void exibir(){
+		No aux = new No();
+		aux = topo;
+		int x;		
+		for(int i = 0; i < cont; i++){
+			System.out.println(aux.getX());
+			aux = aux.getProx();
 		}
 	}
 }
+
--- ep1/Principal.java
+++ ep1/Principal.java
@@ -1,8 +1,9 @@
 public class Principal{
 	public static void main(String[]args){
-		Lista lista1 = new Lista;
+		Lista lista1 = new Lista();
+		lista1.inserir(1);
 		lista1.inserir(10);
-		lista1.inserir(20);
-		lista1.exibirLista();
+		lista1.inserir(100);
+		lista1.exibir();
 	}
 }
