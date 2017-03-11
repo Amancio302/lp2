00a69558dbc092ef82d5c39023909cfe45882397
Amancio302 <Matheusamancioferreira@gmail.com>
2017-03-11 15:50:04 -0300

    ep3 ordenado

--- ep3/Principal.java
+++ ep3/Principal.java
@@ -4,22 +4,27 @@ public class Principal{
 	public static void main(String[] args){
 		Aluno aux;
 		ArrayList<Aluno> alunos = new ArrayList<Aluno>();
-		if(alunos.isEmpty())
-			System.out.println("Vazia");
-		else
-			System.out.println("Não vazia");
-		for(int i = 5; i > 0; i--){
+		for(int i = 0; i < 5; i++){
 			aux = new Aluno();
 			aux.setIdade(i);
 			aux.setNome("José");
 			alunos.add(aux);
 		}
-		if(alunos.isEmpty())
-			System.out.println("Vazia");
-		else
-			System.out.println("Não vazia");
-		System.out.println("Tamanho: " + alunos.size());
-		System.out.println(alunos);
-		System.out.println(alunos.get(0).getIdade());
+                for(int i = 0; i < 5; i++){
+                    for(int j = 0; j < 5; j++){
+                        if(alunos.get(j).getIdade() > alunos.get(i).getIdade()){
+                            aux = new Aluno();
+                            aux.setNome(alunos.get(i).getNome());
+                            aux.setIdade(alunos.get(i).getIdade());
+                            alunos.get(i).setNome(alunos.get(j).getNome());
+                            alunos.get(i).setIdade(alunos.get(j).getIdade());
+                            alunos.get(j).setNome(aux.getNome());
+                            alunos.get(j).setIdade(aux.getIdade());
+                        }
+                    }
+                }
+                for(int i = 0; i < 5; i++){
+                    System.out.println("Nome: " + alunos.get(i).getNome() + " Idade: " + alunos.get(i).getIdade());
+                }
 	}
 }
